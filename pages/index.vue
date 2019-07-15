165a6b2e8d6d46ecca9c378ea2c0b39af322beb6
Stefan Heinemann <stefan.heinemann@liip.ch>
2019-07-15 17:53:49 +0200

    Initialize project

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="~/assets/images/smoke.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+body {
+  background-color: #c7c7c7;
+}
+
+</style>
